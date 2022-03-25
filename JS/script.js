5220b2a2e76f48e6847dd40c1739e188f22f35b9
JeffersonSilemen <jeffersonvieiratec@gmail.com>
2022-03-25 15:17:42 -0300

    Quase la

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,7 @@
-// ATRIBUTOS PERSONALIZADOS
-// let image = document.getElementById('like');
-// image.addEventListener('click', function(){
-// 	// Obtendo atributo da imagem
-// 	// let lastImage = image.getAttribute('src');
-// 	// image.setAttribute('src', 'Images/deslike.jpg')
-// 	// console.log(lastImage);
-// 	// Criando atributos
-// 	// image.setAttribute('cor', 'Vermelho');
-// })
-// Fazendo mais doideras
-// let lista = document.getElementById('lista');
-// let num = parseInt(lista.getAttribute('num'));
-// let conteudo = '';
-// for (let i = 0; i < num; i++){
-// 	conteudo += '<li>' + i + '</li>';
-// }
-// lista.innerHTML = conteudo;
-
-
-// ATRIBUTOS PERSONALIZADOS PARTE 2
-// Pegando atributos com o dataset
-// let lista = document.getElementById('lista');
-// let num = parseInt(lista.dataset.n);
-// console.log(lista.dataset.n)
-// let conteudo = '';
-// for (let i = 0; i < num; i++){
-// 	conteudo += '<li>' + i + '</li>';
-// }
-// lista.innerHTML = conteudo;
-
-
-// VIDEO
-
-
-// VIDEO CONTROLE PARTE1 E PARTE2
 let video = document.getElementById('video1');
+let controls = document.getElementById('controls');
+let container = document.getElementById('content');
+let box_intro = document.getElementById('box_intro');
 function diminuir_vel(){
 	video.playbackRate -= 0.1;
 }
@@ -49,9 +16,20 @@ function avancar(){
 }
 function play(){
 	video.play();
+	video.playbackRate = 1.8;
 }
 function stop(){
 	video.pause();
 	video.currentTime = 0;
-	video.playbackRate = 1;
+	video.playbackRate = 1.8;
+}
+function showControls(){
+	controls.style.display = 'block';
+}
+function hiddenControls(){
+	controls.style.display = 'none';
+}
+function showTheVideo(){
+	box_intro.style.display = 'none';
+	container.style.display = 'flex';
 }
